02e9c46f68b46288299a4e05f2648f689ac198cd
hsivov <hsivov@students.softuni.bg>
2022-07-05 10:08:23 +0300

    More Objects and Classes exercises

--- Fundamentals-Java/ObjectsAndClasses/more/CarSalesman/Car.java
+++ Fundamentals-Java/ObjectsAndClasses/more/CarSalesman/Car.java
@@ -6,6 +6,13 @@ public class Car {
     String weight;
     String color;
 
+    Car(String model, Engine engine, String weight, String color) {
+        this.model = model;
+        this.engine = engine;
+        this.weight = weight;
+        this.color = color;
+    }
+
     public static class Engine {
         String model;
         String power;
@@ -18,5 +25,26 @@ public class Car {
             this.displacement = displacement;
             this.efficiency = efficiency;
         }
+
+        public String getModel() {
+            return model;
+        }
+
+        public String getPower() {
+            return power;
+        }
+
+        public String getDisplacement() {
+            return displacement;
+        }
+
+        public String getEfficiency() {
+            return efficiency;
+        }
+    }
+    public String toString() {
+        return String.format("%s:%n  %s:%n    Power: %s%n    Displacement: %s%n" +
+                "    Efficiency: %s%n  Weight: %s%n  Color: %s", this.model, this.engine.getModel(), this.engine.getPower(),
+                this.engine.getDisplacement(), this.engine.getEfficiency(), this.weight, this.color);
     }
 }
--- Fundamentals-Java/ObjectsAndClasses/more/CarSalesman/Main.java
+++ Fundamentals-Java/ObjectsAndClasses/more/CarSalesman/Main.java
@@ -8,34 +8,61 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         List<Car.Engine> engineList = new ArrayList<>();
+        List<Car> cars = new ArrayList<>();
 
         int n = Integer.parseInt(scanner.nextLine());
 
         for (int line = 1; line <= n; line++) {
-            String[] input = scanner.nextLine().split(" ");
-            String engineModel = input[0];
-            String enginePower = input[1];
+            String[] inputEngine = scanner.nextLine().split(" ");
+            String engineModel = inputEngine[0];
+            String enginePower = inputEngine[1];
             String engineDisplacement = "n/a";
             String engineEfficiency = "n/a";
 
-            if (input.length == 3){
-                if (Character.isDigit(input[2].charAt(0))) {
-                    engineDisplacement = input[2];
+            if (inputEngine.length == 3){
+                if (Character.isDigit(inputEngine[2].charAt(0))) {
+                    engineDisplacement = inputEngine[2];
                 } else {
-                    engineEfficiency = input[2];
+                    engineEfficiency = inputEngine[2];
                 }
             }
-            if (input.length == 4){
-                engineDisplacement = input[2];
-                engineEfficiency = input[3];
+            if (inputEngine.length == 4){
+                engineDisplacement = inputEngine[2];
+                engineEfficiency = inputEngine[3];
             }
             Car.Engine engine = new Car.Engine(engineModel, enginePower, engineDisplacement, engineEfficiency);
             engineList.add(engine);
         }
         int m = Integer.parseInt(scanner.nextLine());
         for (int line = 1; line <= m; line++) {
-            String[] input = scanner.nextLine().split(" ");
+            String[] inputCar = scanner.nextLine().split(" ");
+            String model = inputCar[0];
+            String engine = inputCar[1];
+            String weight = "n/a";
+            String color = "n/a";
+            Car.Engine currentEngine = null;
 
+            if (inputCar.length == 3){
+                if (Character.isDigit(inputCar[2].charAt(0))) {
+                    weight = inputCar[2];
+                } else {
+                    color = inputCar[2];
+                }
+            }
+            if (inputCar.length == 4){
+                weight = inputCar[2];
+                color = inputCar[3];
+            }
+            for (Car.Engine engineModel : engineList){
+                if (engineModel.getModel().equals(engine)){
+                    currentEngine = engineModel;
+                }
+            }
+            Car car = new Car(model, currentEngine, weight, color);
+            cars.add(car);
+        }
+        for (Car car : cars){
+            System.out.println(car);
         }
     }
 }
